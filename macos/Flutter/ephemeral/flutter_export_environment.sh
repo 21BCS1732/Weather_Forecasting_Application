5493096895ad9946790e33b38d2f24bda55fa4ff
Gargi Sinha <122735009+21BCS1732@users.noreply.github.com>
2023-12-26 22:15:18 +0530

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/kushagra/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/kushagra/New Volume1/Rain-main"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.2.7"
+export "FLUTTER_BUILD_NUMBER=30"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
